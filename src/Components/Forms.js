2c21e5bcaa3a171e7d9a837ecc999a764967b740
Austin Greer <agreer.ag12@gmail.com>
2021-02-27 01:54:40 -0500

    building form

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import pizzaImg from '../Assets/Pizza.jpg';
+
 
 
 
 const initialForm = {
+    name: '',
+    
     size: {
         small: false,
         medium: false,
         large: false
     },
-  
-    sauce: {
-        originalRed: false,
-        creamyGarlic: false,
-        spinachAlfredo: false
-    },
-  
+
     toppings: {
         pepperoni: false,
         sausage: false,
@@ -31,11 +29,79 @@ const initialForm = {
 
 
 export default function Forms () {
+    const [forms, setForms] = useState(initialForm)
+    const [error, setError] = useState({
+        name: ''
+    })
+    
+    const change = (event) => {
+
+    }
+    
     return(
         <div>
             <div className='navigation'>
                 <Link to='/'>Home</Link>
                 <Link to='/pizza'>Place Order</Link>
+            </div>
+            <div className='forms-container'>
+                <h1>Build Your own Pizza</h1>
+                <img src={pizzaImg}></img>
+                <form>
+                    {/* Name input */}
+                
+                        <h2>Name:</h2>
+                        <input 
+                            name='name'
+                            type='text'
+                            placeHolder='enter your name'
+                            onChange={change}
+                        />
+                    
+                    {/* dropdown */}
+                    
+                        <h2>Select Crust Size</h2>
+                        <select>
+                            <option>--Select Size--</option>
+                            <option>Small</option>
+                            <option>Medium</option>
+                            <option>Large</option>
+                        </select>
+                    
+
+                    {/* check boxes */}
+                    <h2>Choose Toppings:</h2>
+                    <label>
+                        pepperoni <input type='checkbox'/>
+                    </label>
+                    <label>
+                        sausage <input type='checkbox'/>
+                    </label>
+                    <label>
+                        bacon <input type='checkbox'/>
+                    </label>
+                    <label>
+                        green pepper <input type='checkbox'/>
+                    </label>
+                    <label>
+                        red onion <input type='checkbox'/>
+                    </label>
+                    <label>
+                        olives <input type='checkbox'/>
+                    </label>
+                    <label>
+                        mushroom <input type='checkbox'/>
+                    </label>
+                    <input
+                        type='text'
+                        name='instructions'
+                        placeholder='special instructions'
+                    />
+                    <input
+                        type='submit'
+                    />
+                    
+                </form>
             </div>
         </div>
         
